531a3df8e4f432f5933181b7f7b6d421a9961c70
Levin <levin.fankhauser@gmx.ch>
2026-03-23 09:27:48 +0100

    Fix sign out behaviour

--- components/layout/SignOutButton.tsx
+++ components/layout/SignOutButton.tsx
@@ -16,11 +16,10 @@ export default function SignOutButton() {
   return (
     <button
       className="rounded-lg border border-rose-200 bg-rose-50 px-4 py-2 text-sm text-rose-700 transition hover:border-rose-300 hover:bg-rose-100 dark:border-rose-300/30 dark:bg-rose-400/10 dark:text-rose-100 dark:hover:border-rose-300/70 dark:hover:bg-rose-400/20"
-      onClick={() =>
-        void signOut().then(() => {
-          router.push("/signin");
-        })
-      }
+      onClick={() => {
+        void signOut();
+        router.push("/signin");
+      }}
     >
       Abmelden
     </button>
